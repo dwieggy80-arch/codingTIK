7b2fd0c09838795be2719862912ac9967e1c7e7d
dwieggy80-arch <dwieggy80@gmail.com>
2025-10-07 12:58:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 const tombolSapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
-    // 3. Jalankan kode ini etika tombol di-klik
-    alert('Halo! Thank you very much! );
+    // 3. Jalankan kode ini ketika tombol di-klik
+    alert('Halo! Terima kasih sudah berkunjung!' );
           });
