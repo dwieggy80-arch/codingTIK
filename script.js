dd20e601a2c236a86a137c554b5b07893ef3fce2
dwieggy80-arch <dwieggy80@gmail.com>
2025-10-07 12:32:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. Pilih tombol berdasarkan ID-nya
-cost tombolSapa = document.getElementByld('sapaButton');
+cost tombolSapa = document.getElementByld("sapaButton");
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', functioun() {
     // 3. Jalankan kode ini etika tombol di-klik
