9fbbb1e59395831f5eabe419baeb6932de9f2164
dwieggy80-arch <dwieggy80@gmail.com>
2025-10-07 12:51:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = document.getElementByld('sapaButton');
+const tombolSapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
-tombolSapa.addEventListener('click', functioun() {
+tombolSapa.addEventListener('click', function() {
     // 3. Jalankan kode ini etika tombol di-klik
     alert('Halo! Thank you very much! );
           });
